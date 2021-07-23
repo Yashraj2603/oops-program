ea0b73b28d57f3e3042f398c7153587ec5786f61
Yash Raj <55595124+Yashraj2603@users.noreply.github.com>
2021-07-23 17:20:18 +0530

    default constructor example

---
+++ default_constructorexample.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+Class Employee
+ {
+public:
+Employee()
+{
+cout<<"Default constructor invoked";
+}
+ };
+int main()
+{
+  Employee e1;
+  Employee e2;
+  return 0;
+}
